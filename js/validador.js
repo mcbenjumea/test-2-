7e56da259bb6617d1a777db884c00ade337722ad
mcbenjumea <mcblacouture@gmail.com>
2026-04-04 12:53:21 -0300

    config mensaje

--- js/validador.js
+++ js/validador.js
@@ -23,3 +23,29 @@ export const uiPampa = {
     },
     limpiarFormulario: (form) => form.reset()
 };
+
+function gestionarEnvioPampa() {
+    const nombreCliente = document.querySelector('#nombre').value;
+    const correoCliente = document.querySelector('#email').value;
+
+    if (nombreCliente === "" || correoCliente === "") {
+        alert("No se puede enviar la solicitud: el nombre o el correo están vacíos.");
+        return false;
+    }
+
+    alert(`Solicitud enviada para ${nombreCliente}. Nos contactaremos pronto con noticias de nuestras sedes en Palermo, Almagro o Caballito.`);
+    return true;
+}
+
+function gestionarEnvioPampa () {
+const descripcionMensaje = document.querySelector('#mensaje').value;
+
+ if (descripcionMensaje === "" || mensaje === "") {
+        alert("No se puede enviar la solicitud: el mensaje está vacío.");
+        return false;
+    }
+
+    alert(`Solicitud enviada para ${descripcionMensaje}. Nos contactaremos pronto con noticias de nuestras sedes en Palermo, Almagro o Caballito.`);
+    return true;
+
+}
